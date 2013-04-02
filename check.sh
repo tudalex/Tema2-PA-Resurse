1032b7cd8177e36c39f3a9be63b86674a8f524e9
Constantin A. Tudorica <tudalex@gmail.com>
2013-04-02 20:07:48 +0300

    Am adaugat timeout-ul.

--- check.sh
+++ check.sh
@@ -16,7 +16,7 @@ points=(10 15 20)
 total=0
 for i in $(seq 0 2); do
 	echo -n "Verificam cu nivelul de inteligenta $i cu botul in pozitia 1:" 
-	./server.py "$BOT B $SEED" "$OUR_BOT A $i $SEED" &>/dev/null
+	./server.py -t $TIMEOUT "$BOT B $SEED" "$OUR_BOT A $i $SEED" &>/dev/null
 	rc=$?
 	if [[ $rc == 61 ]]; then
 		total=$(( ${points[$i]} + $total))
@@ -29,7 +29,7 @@ for i in $(seq 0 2); do
 	cp viewer_log "viewer_log_${i}_${rc}"
 	
 	echo -n "Verificam cu nivelul de inteligenta $i cu botul in pozitia 2:" 
-	./server.py "$OUR_BOT A $i $SEED" "$BOT B $SEED" &>/dev/null
+	./server.py -t $TIMEOUT "$OUR_BOT A $i $SEED" "$BOT B $SEED" &>/dev/null
 	rc=$?
 	if [[ $rc == 62 ]]; then
 		total=$(( ${points[$i]} + $total))
